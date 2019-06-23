af3809c6e48be52e29e803cf05dd96d4ed67ef50
zidol <ajh3166@naver.com>
2019-06-23 10:14:35 +0900

    Auth Actions 추가

--- src/module/auth/actions.js
+++ src/module/auth/actions.js
@@ -1,14 +1,20 @@
 import { createAction } from 'redux-actions'; 
 import * as authAPI from '../../api/authAPI'; 
 import * as types from '../actionTypes'; 
+
+
+/**
+ * 로그인 유저 갱신
+ */
+export const updateUser = createAction(types.UPDATE_USER)
  
 /** 
  * 이메일 로그인 
  */ 
  
-const signInWithEmailRequest = createAction(types.SIGN_IN_WITH_EMAIL_REQUEST) 
-const signInWithEmailSuccess = createAction(types.SIGN_IN_WITH_EMAIL_SUCCESS) 
-const signInWithEmailFailed = createAction(types.SIGN_IN_WITH_EMAIL_FAILED) 
+const signInWithEmailRequest = createAction(types.SIGN_IN_WITH_EMAIL_REQUEST);
+const signInWithEmailSuccess = createAction(types.SIGN_IN_WITH_EMAIL_SUCCESS); 
+const signInWithEmailFailed = createAction(types.SIGN_IN_WITH_EMAIL_FAILED);
  
 export const signInWithEmail = (email, password) => { 
     return (dispatch) => { 
@@ -23,3 +29,90 @@ export const signInWithEmail = (email, password) => {
     } 
 } 
  
+
+/**
+ * 페이스북 로그인
+ */
+
+ const signInWithFacebookRequest = createAction(types.SIGN_IN_WITH_FACEBOOK_REQUEST);
+ const signInWithFacebookSuccess = createAction(types.SIGN_IN_WITH_FACEBOOK_SUCCESS);
+ const signInWithFacebookFailed = createAction(types.SIGN_IN_WITH_FACEBOOK_FAILED);
+
+
+ export const signInWithFacebook = () => { 
+    return (dispatch) => { 
+        dispatch(signInWithFacebookRequest()); 
+        authAPI.signInWithFacebook()
+            .then(() => { 
+                dispatch(signInWithFacebookSuccess()) 
+            }) 
+            .catch((error) => { 
+                dispatch(signInWithFacebookFailed(error)) 
+            }) 
+    } 
+} 
+ 
+
+/**
+ * 구글 로그인
+ */
+
+const signInWithGoogleRequest = createAction(types.SIGN_IN_WITH_GOOGLE_REQUEST);
+const signInWithGoogleSuccess = createAction(types.SIGN_IN_WITH_GOOGLE_SUCCESS);
+const signInWithGoogleFailed = createAction(types.SIGN_IN_WITH_GOOGLE_FAILED);
+
+
+export const signInWithGoogle = () => { 
+   return (dispatch) => { 
+       dispatch(signInWithGoogleRequest()); 
+       authAPI.signInWithGoogle() 
+           .then(() => { 
+               dispatch(signInWithGoogleSuccess()) 
+           }) 
+           .catch((error) => { 
+               dispatch(signInWithGoogleFailed(error)) 
+           }) 
+   } 
+} 
+
+/**
+ * 이메일 회원가입
+ */
+
+const signUpWithEmailRequest = createAction(types.SIGN_UP_WITH_EMAIL_REQUEST);
+const signUpWithEmailSuccess = createAction(types.SIGN_UP_WITH_EMAIL_SUCCESS); 
+const signUpWithEmailFailed = createAction(types.SIGN_UP_WITH_EMAIL_FAILED);
+ 
+export const signUpWithEmail = (email, password) => { 
+    return (dispatch) => { 
+        dispatch(signUpWithEmailRequest()); 
+        authAPI.signUpWithEmail(email, password) 
+            .then(() => { 
+                dispatch(signUpWithEmailSuccess()) 
+            }) 
+            .catch((error) => { 
+                dispatch(signUpWithEmailFailed(error)) 
+            }) 
+    } 
+} 
+
+/**
+ * 로그아웃
+ */
+
+const signOutRequest = createAction(types.SIGN_OUT_REQUEST); 
+const signOutSuccess = createAction(types.SIGN_OUT_SUCCESS);
+const signOutFailed = createAction(types.SIGN_OUT_FAILED);
+ 
+export const signOut = () => { 
+    return (dispatch) => { 
+        dispatch(signOutRequest()); 
+        authAPI.signOutRequest() 
+            .then(() => { 
+                dispatch(signOutSuccess()) 
+            }) 
+            .catch((error) => { 
+                dispatch(signOutFailed(error)) 
+            }) 
+    } 
+} 
